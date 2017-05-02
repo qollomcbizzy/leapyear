86a7cc0988881b1c61d38393d2e8c132de0b58a4
collo <qollomcbizzy64@gmail.com>
2017-05-02 15:00:24 +0300

    refactored the code into one sentence

--- Js/script.js
+++ Js/script.js
@@ -1,10 +1,7 @@
 var leapYear = function(year){
-   if(year/100 === 0){
-     return false;
+   if(year % 4 === 0  && year/100 !== 0){
+     return true;
    }
-  else if(year % 4 === 0){
-    return true;
-  }
   else{
   return false;
     }
